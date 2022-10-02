39b3099226f7d120219060add2a9b78def91a22a
Sachin Holla <sacholla@amazon.com>
2022-10-02 00:18:46 -0700

    Added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "notesappimages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
